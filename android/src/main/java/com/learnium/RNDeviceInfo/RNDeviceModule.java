13cd32c4a316a88ab22597334224ee31a89c8038
peteclark3 <peter.r.clark@gmail.com>
2016-05-25 10:31:50 -0400

    import instance id

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings.Secure;
 
+import com.google.android.gms.iid.InstanceID;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
