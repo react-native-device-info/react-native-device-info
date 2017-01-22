2b3c8c11166c398e3dc7353220398a638035f210
Igor Yegorov <iegoroff@gmail.com>
2017-01-22 03:34:00 +0300

    added typings

---
+++ deviceinfo.d.ts
@@ -0,0 +1,26 @@
+// should be imported this way: 
+// import DeviceInfo from 'react-native-device-info';
+
+declare class DeviceInfo {
+  public static getUniqueID(): string;
+  public static getManufacturer(): string;
+  public static getBrand(): string;
+  public static getModel(): string;
+  public static getDeviceId(): string;
+  public static getSystemName(): string;
+  public static getSystemVersion(): string;
+  public static getBundleId(): string;
+  public static getBuildNumber(): string;
+  public static getVersion(): string;
+  public static getReadableVersion(): string;
+  public static getDeviceName(): string;
+  public static getUserAgent(): string;
+  public static getDeviceLocale(): string;
+  public static getDeviceCountry(): string;
+  public static getTimezone(): string;
+  public static getInstanceID(): string;
+  public static isEmulator(): boolean;
+  public static isTablet(): boolean;
+}
+
+export default DeviceInfo;
