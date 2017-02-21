e7a4bec64645c1c34a90efb9260016f282528cba
Jared Forsyth <jared@jaredforsyth.com>
2017-02-21 13:55:59 -0700

    remove google play services dep

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -7,8 +7,6 @@ import android.content.res.Configuration;
 import android.os.Build;
 import android.provider.Settings.Secure;
 
-import com.google.android.gms.iid.InstanceID;
-
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
@@ -97,7 +95,6 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
       e.printStackTrace();
     }
 
-    constants.put("instanceId", InstanceID.getInstance(this.reactContext).getId());
     constants.put("deviceName", deviceName);
     constants.put("systemName", "Android");
     constants.put("systemVersion", Build.VERSION.RELEASE);
