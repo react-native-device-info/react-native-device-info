920d0e0d12dad120a9680d248109fb4f054ff450
zhouxuemeng1988 <zhouxuemeng@gochinatv.com>
2017-11-02 01:14:54 -0500

    Some Devices may have Wired internet connection
    Some Devices may have Wired internet connection,and May have No WifiInfo connection.
    For example, some android OTT and android TV.

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -25,6 +25,10 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import javax.annotation.Nullable;
 
@@ -103,9 +107,67 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void getMacAddress(Promise p) {
-    String macAddress = getWifiInfo().getMacAddress();
+//     String macAddress = getWifiInfo.getMacAddress();
+    String macAddress = getMacAddress();
     p.resolve(macAddress);
   }
+  
+   public static String getMacAddress() {
+        String strMacAddr = "";
+        try {
+            InetAddress ip = getLocalInetAddress();
+
+            byte[] b = NetworkInterface.getByInetAddress(ip)
+                    .getHardwareAddress();
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                if (i != 0) {
+                    buffer.append(':');
+                }
+
+                String str = Integer.toHexString(b[i] & 0xFF);
+                buffer.append(str.length() == 1 ? 0 + str : str);
+            }
+            strMacAddr = buffer.toString().toUpperCase();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return strMacAddr;
+    }
+
+    /**
+     * get local IP address
+     *
+     * @return
+     */
+    protected static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            //列举
+            Enumeration en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {//是否还有元素
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
+                Enumeration en_ip = ni.getInetAddresses();//得到一个ip地址的列举
+                while (en_ip.hasMoreElements()) {
+                    ip = (InetAddress) en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1)
+                        break;
+                    else
+                        ip = null;
+                }
+
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+
+            e.printStackTrace();
+        }
+        return ip;
+    }
 
   @Override
   public @Nullable Map<String, Object> getConstants() {
