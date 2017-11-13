e30a3c3a352edabc8c44c9071aadec9097ea2a6c
Adric <adric.landman@gmail.com>
2017-11-13 17:17:16 +1100

    Removed an unused dependency
    This dependency is not required. It also stops the project from being used.

--- windows/RNDeviceInfo/RNDeviceInfoModule.cs
+++ windows/RNDeviceInfo/RNDeviceInfoModule.cs
@@ -7,7 +7,6 @@ using System.Text.RegularExpressions;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using WinRTXamlToolkit.Controls;
 
 namespace RNDeviceInfo
 {
