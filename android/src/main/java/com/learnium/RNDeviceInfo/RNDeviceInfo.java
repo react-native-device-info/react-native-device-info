3723d3a1cf9161a6700087ab0dc7ca5c8bd9f675
ZJ <ReachZJLin@gmail.com>
2018-07-26 17:46:06 -0700

    Update RNDeviceInfo.java
    removed deprecated createJSModule

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceInfo.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceInfo.java
@@ -21,11 +21,6 @@ public class RNDeviceInfo implements ReactPackage {
     return modules;
   }
 
-  // Deprecated RN 0.47
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
   @Override
   public List<ViewManager> createViewManagers(
       ReactApplicationContext reactContext) {
