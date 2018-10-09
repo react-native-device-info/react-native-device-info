c41be13a394900fb8567b513d5a639300db0fce2
Ayush Bansal <ayushbansal@Ayushs-MacBook-Pro.local>
2018-10-09 12:31:07 +0530

    fix/added callback instead of promise

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -212,13 +212,13 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void isBatteryCharging(Promise p){
+  public void isBatteryCharging(Callback callback){
     IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
     Intent batteryStatus = this.reactContext.getApplicationContext().registerReceiver(null, ifilter);
     int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
     boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
                      status == BatteryManager.BATTERY_STATUS_FULL;
-    p.resolve(isCharging);
+    callback.invoke(isCharging);
   }
 
   @ReactMethod
