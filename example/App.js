17c0bfcf72d4a43ab4faf5ccd936351741822268
Mike Hardy <github@mikehardy.net>
2019-03-11 15:03:14 -0500

    docs: Add example demonstrating usage (#589)
    * Install of default `react-native init example` scaffold
    
    * What the example looks like after react-native link
    
    * Fully integrated example app
    
    * Implement automated example construction - refresh any time
    
    * Ignore the example and associated refresh script

---
+++ example/App.js
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, ScrollView, StyleSheet, Text, View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      deviceinfo: {},
+    };
+  }
+
+  async componentWillMount() {
+    let deviceJSON = {};
+    const ios = Platform.OS === 'ios';
+
+    try {
+
+      deviceJSON.uniqueID = DeviceInfo.getUniqueID();
+      deviceJSON.manufacturer = DeviceInfo.getManufacturer();
+      deviceJSON.brand = DeviceInfo.getBrand();
+      deviceJSON.model = DeviceInfo.getModel();
+      deviceJSON.deviceId = DeviceInfo.getDeviceId();
+      deviceJSON.systemName = DeviceInfo.getSystemName();
+      deviceJSON.systemVersion = DeviceInfo.getSystemVersion();
+      deviceJSON.bundleId = DeviceInfo.getBundleId();
+      deviceJSON.buildNumber = DeviceInfo.getBuildNumber();
+      deviceJSON.version = DeviceInfo.getVersion();
+      deviceJSON.readableVersion = DeviceInfo.getReadableVersion();
+      deviceJSON.deviceName = DeviceInfo.getDeviceName(); // needs android.permission.BLUETOOTH ?
+      deviceJSON.userAgent = DeviceInfo.getUserAgent();
+      deviceJSON.deviceLocale = DeviceInfo.getDeviceLocale();
+      deviceJSON.deviceCountry = DeviceInfo.getDeviceCountry();
+      deviceJSON.timezone = DeviceInfo.getTimezone();
+      deviceJSON.instanceID = ios ? '' : DeviceInfo.getInstanceID();
+      deviceJSON.installReferrer = ios ? '' : DeviceInfo.getInstallReferrer();
+      deviceJSON.isEmulator = DeviceInfo.isEmulator();
+      deviceJSON.isTablet = DeviceInfo.isTablet();
+      deviceJSON.fontScale = DeviceInfo.getFontScale();
+      deviceJSON.hasNotch = DeviceInfo.hasNotch();
+      deviceJSON.firstInstallTime = ios ? -1 : DeviceInfo.getFirstInstallTime();
+      deviceJSON.lastUpdateTime = ios ? -1 : DeviceInfo.getLastUpdateTime();
+      deviceJSON.serialNumber = ios ? -1 : DeviceInfo.getSerialNumber();
+      deviceJSON.IPAddress = await DeviceInfo.getIPAddress();
+      deviceJSON.MACAddress = await DeviceInfo.getMACAddress(); // needs android.permission.ACCESS_WIFI_STATE ?
+      deviceJSON.phoneNumber = ios ? '' : DeviceInfo.getPhoneNumber(); // needs android.permission.READ_PHONE_STATE ?
+      deviceJSON.APILevel = ios ? -1 : DeviceInfo.getAPILevel();
+      deviceJSON.carrier = DeviceInfo.getCarrier();
+      deviceJSON.totalMemory = DeviceInfo.getTotalMemory();
+      deviceJSON.maxMemory = ios ? -1 : DeviceInfo.getMaxMemory();
+      deviceJSON.totalDiskCapacity = DeviceInfo.getTotalDiskCapacity(); // FIXME needs a patch for integer overflow on Android
+      deviceJSON.freeDiskStorage = DeviceInfo.getFreeDiskStorage(); // FIXME needs a patch for integer overflow on Android
+      deviceJSON.batteryLevel = await DeviceInfo.getBatteryLevel();
+      deviceJSON.isLandscape = DeviceInfo.isLandscape();
+      deviceJSON.isAirplaneMode = ios ? false : await DeviceInfo.isAirPlaneMode();
+      deviceJSON.isBatteryCharging = ios ? false : await DeviceInfo.isBatteryCharging();
+      deviceJSON.deviceType = DeviceInfo.getDeviceType();
+      deviceJSON.isPinOrFingerprintSet = 'unknown';
+    } catch (e) {
+      console.log('Trouble getting device info ', e);
+    }
+
+    DeviceInfo.isPinOrFingerprintSet()(this.keyguardCallback);
+
+    console.log('loaded info');
+    this.setState({ deviceinfo: deviceJSON });
+    this.forceUpdate();
+    console.log(this.state.deviceinfo);
+  }
+
+  keyguardCallback = (pinSet) => {
+    console.log('callback called with value: ' + pinSet);
+    let deviceJSON = this.state.deviceinfo;
+    deviceJSON.isPinOrFingerprintSet = pinSet;
+    this.setState({ deviceinfo: deviceJSON });
+    this.forceUpdate();
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>react-native-device-info example - info:</Text>
+        <ScrollView>
+          <Text style={styles.instructions}>{JSON.stringify(this.state.deviceinfo, null, '\t')}</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'left',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
