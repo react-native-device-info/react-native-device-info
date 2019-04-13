89007dbb026a53b94438dd1aa093c9246fe89e52
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-04-13 21:02:15 +0430

    Add buildId to windows bridge
    is not available

--- windows/RNDeviceInfo/RNDeviceInfoModule.cs
+++ windows/RNDeviceInfo/RNDeviceInfoModule.cs
@@ -172,6 +172,7 @@ namespace RNDeviceInfo
                 constants["apiLevel"] = "not available";
                 constants["model"] = model;
                 constants["brand"] = model;
+                constants["buildId"] = "not available";
                 constants["deviceId"] = hardwareVersion;
                 constants["deviceLocale"] = culture.Name;
                 constants["deviceCountry"] = culture.EnglishName;
