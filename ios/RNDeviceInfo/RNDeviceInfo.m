00e7da4a9ecd56b50dc2dee1baa6f568c045f5bf
Gustavo Genovese <gcesarmza@gmail.com>
2019-10-03 10:48:31 -0300

    improved code readability

--- ios/RNDeviceInfo/RNDeviceInfo.m
+++ ios/RNDeviceInfo/RNDeviceInfo.m
@@ -389,11 +389,12 @@ RCT_EXPORT_METHOD(getSupportedAbis:(RCTPromiseResolveBlock)resolve rejecter:(RCT
                         [ifname isEqualToString:@"en0"] ||
                         // Check if interface is en1 which is the wifi connection on the Apple TV
                         [ifname isEqualToString:@"en1"]
-                    ) {
+                    )
                 #else
                     // Check if interface is en0 which is the wifi connection on the iPhone
-                    if([ifname isEqualToString:@"en0"]) {
+                    if([ifname isEqualToString:@"en0"])
                 #endif
+                    {
                         // Get NSString from C String
                         address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
                     }
