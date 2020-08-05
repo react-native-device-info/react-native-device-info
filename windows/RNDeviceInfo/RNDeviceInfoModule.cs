d5ac3955420625d2ab8e6cd6511c8a61be6d515a
Tero Paananen <tero.paananen@showell.com>
2020-08-05 15:33:50 +0300

    Porting code started

--- windows/RNDeviceInfo/RNDeviceInfoModule.cs
+++ windows/RNDeviceInfo/RNDeviceInfoModule.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using Windows.Security.Credentials.UI;
 
 using Microsoft.ReactNative.Managed;
 using Microsoft.ReactNative;
@@ -12,5 +15,66 @@ namespace RNDeviceInfo
     [ReactModule("RNDeviceInfo")]
     class RNDeviceInfoModule
     {
+        // TODO: how return constants?
+        /*
+        public override JObject ModuleConstants
+        {
+            get
+            {
+                return new JObject
+                {
+                    { "uniqueId", getUniqueIdSync() },
+                    { "deviceId", getDeviceIdSync() },
+                    { "bundleId", getBundleIdSync() },
+                    { "systemVersion", getSystemVersionSync() },
+                    { "appVersion", getAppVersionSync() },
+                    { "buildNumber", getBuildNumberSync() },
+                    { "isTablet", isTabletSync() },
+                    { "appName", getAppNameSync() },
+                    { "brand", getBrandSync() },
+                    { "model", getModelSync() },
+                };
+            }
+        } */
+
+        private bool IsEmulator(string model)
+        {
+            Regex rgx = new Regex("(?i:virtual)");
+            return rgx.IsMatch(model);
+        }
+
+        private bool IsTablet(string os)
+        {
+            Regex rgx = new Regex("(?i:windowsphone)");
+            return !rgx.IsMatch(os);
+        }
+
+        private async Task<bool> isPinOrFingerprint()
+        {
+            try
+            {
+                var ucvAvailability = await UserConsentVerifier.CheckAvailabilityAsync();
+                return ucvAvailability == UserConsentVerifierAvailability.Available;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        [ReactMethod]
+        public bool isPinOrFingerprintSetSync()
+        {
+            // TODO: Is this synchronous method?
+            return isPinOrFingerprint().Result;
+        }
+
+        [ReactMethod]
+        public async void isPinOrFingerprintSet(IReactPromise<bool> result)
+        {
+            var ret = await isPinOrFingerprint();
+            result.Resolve(ret);
+        }
+
     }
 }
