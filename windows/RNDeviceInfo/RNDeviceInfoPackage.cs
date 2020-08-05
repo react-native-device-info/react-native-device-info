6db96c0be61249cdbac335563c0b96fb3b4bee7b
Tero Paananen <tero.paananen@showell.com>
2020-08-05 14:35:32 +0300

    Base for new Windows next module

--- windows/RNDeviceInfo/RNDeviceInfoPackage.cs
+++ windows/RNDeviceInfo/RNDeviceInfoPackage.cs
@@ -1,30 +1,12 @@
-﻿using ReactNative.Bridge;
-using ReactNative.Modules.Core;
-using ReactNative.UIManager;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RNDeviceInfo
 {
-    public class RNDeviceInfoPackage : IReactPackage
+    public sealed class RNDeviceInfoPackage
     {
-        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
-        {
-            return new List<INativeModule>
-            {
-                new RNDeviceInfoModule(reactContext)
-            };
-        }
-
-        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
-        {
-            return new List<Type>(0);
-        }
-
-        public IReadOnlyList<IViewManager> CreateViewManagers(
-            ReactContext reactContext)
-        {
-            return new List<IViewManager>(0);
-        }
     }
 }
