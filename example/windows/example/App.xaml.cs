eb208cd69db8e8ecd212b5d837c9850058f4c97f
Tero Paananen <tero.paananen@showell.com>
2020-08-20 12:13:23 +0300

    Windows app base for example app

---
+++ example/windows/example/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ReactNative;
+
+namespace example
+{
+    sealed partial class App : ReactApplication
+    {
+        public App()
+        {
+            MainComponentName = "example";
+
+#if BUNDLE
+            JavaScriptBundleFile = "index.windows";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = false;
+#else
+            JavaScriptMainModuleName = "index";
+            InstanceSettings.UseWebDebugger = true;
+            InstanceSettings.UseFastRefresh = true;
+#endif
+
+#if DEBUG
+            InstanceSettings.EnableDeveloperMenu = true;
+#else
+            InstanceSettings.EnableDeveloperMenu = false;
+#endif
+
+            Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
+            
+            PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider()); // Includes any modules in this project
+
+            InitializeComponent();
+        }
+    }
+}
