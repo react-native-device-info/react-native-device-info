799aefcb8c6972197318da5bbb497fa1360da03d
Dustin Schie <dustin@schie.io>
2020-10-24 23:12:57 -0400

    test(__tests__): adding tests
    - adds tests for async-hook-wrappers and a getter
    - sets `test` script in package.json
    
    re #1108

---
+++ __tests__/async-hook-wrappers.test.ts
@@ -0,0 +1,76 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useOnMount, useOnEvent, deviceInfoEmitter } from '../src/internal/asyncHookWrappers';
+
+describe('async-hook-wrappers', () => {
+  let asyncGetter = jest.fn(() => Promise.resolve(true));
+  let initialValue = false;
+  beforeEach(() => {
+    asyncGetter.mockClear();
+    initialValue = Math.random() > 0.5;
+  });
+
+  describe('useOnMount', () => {
+    it('should exist as a function', () => {
+      expect(typeof useOnMount).toBe('function');
+    });
+    it('should return initial value before effect load', async () => {
+      const { result, waitForNextUpdate, unmount } = renderHook(() =>
+        useOnMount(asyncGetter, initialValue)
+      );
+      expect(result.current).toEqual({ loading: true, result: initialValue });
+      await waitForNextUpdate();
+    });
+
+    it('should return a new Value after async result obtained', async () => {
+      const { result, waitForNextUpdate } = renderHook(() => useOnMount(asyncGetter, initialValue));
+      await waitForNextUpdate();
+
+      expect(asyncGetter).toHaveBeenCalled();
+      expect(result.current).toEqual({ loading: false, result: true });
+    });
+  });
+
+  describe('useOnEvent', () => {
+    const eventName = 'event-hook-event';
+    beforeEach(() => {
+      deviceInfoEmitter.removeAllListeners(eventName);
+    });
+    beforeAll((done) => {
+      done();
+    });
+    it('should exist as a function', () => {
+      expect(typeof useOnEvent).toBe('function');
+    });
+
+    it('should return a default value before effect load', async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useOnEvent(eventName, asyncGetter, initialValue)
+      );
+      expect(result.current).toEqual({ loading: true, result: initialValue });
+      await waitForNextUpdate();
+    });
+
+    it('should return a new Value after async result obtained', async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useOnEvent(eventName, asyncGetter, initialValue)
+      );
+      await waitForNextUpdate();
+
+      expect(asyncGetter).toHaveBeenCalled();
+      expect(result.current).toEqual({ loading: false, result: true });
+    });
+
+    it('should return a new Value after event', async () => {
+      const { result, waitForNextUpdate, rerender } = renderHook(() =>
+        useOnEvent(eventName, asyncGetter, initialValue)
+      );
+      const value = Math.random();
+      act(() => {
+        deviceInfoEmitter.emit(eventName, value);
+        rerender();
+      });
+      await waitForNextUpdate();
+      expect(result.current).toEqual({ loading: false, result: value });
+    });
+  });
+});
