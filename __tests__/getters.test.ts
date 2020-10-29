5003f1e36fa2625939029a65decaad65c7831c3c
Dustin Schie <schie@users.noreply.github.com>
2020-10-29 09:06:46 -0400

    tests: memoized string getters and supported-platform-info (#1114)
    * test(__tests__/supported-platform-info): testing supported-platform-info file
    
    adds tests for exported functions of the file, `supported-platform-info.ts`
    
    re #1108
    
    * refactor(__tests/getters): rewriting string getter tests to be more scalable
    
    rewrites tests to simplify the testing process
    
    * test(__tests__/getters): adding rest of memoized string getters
    
    adds the following to testing:
    - getSerialNumber
    - getAndroidId
    - getBuildId
    - getInstallerPackageName
    - getDeviceName
    - getBootloader
    - getDevice
    - getDisplay
    - getFingerprint
    - getHardware
    - getHost
    - getProduct
    - getTags
    - getType
    - getBaseOs
    - getSecurityPatch
    - getCodename
    - getIncremental
    - getInstallReferrer
    
    re #1108
    
    * test(jest.setup.ts): putting `getInstallReferrer` in correct spot
    
    moves `getInstallReferrer` from `numberFnNames` to `stringFnNames`
    
    * test(__tests__/getters): casting `RNDeviceInfo` as `any`
    
    updates `makeTable` fn to cast `RnDeviceInfo` usage as `any` to prevent GitHub Action testing from
    failing
    
    * test(__tests__/supported-platform-info): changing memo key for `getSupportedPlatformInfoAsync` tests
    
    updates tests for `getSupportedPlatformInfoAsync` to use the memo key,
    `getSupportedPlatformInfoAsync`

--- __tests__/getters.test.ts
+++ __tests__/getters.test.ts
@@ -1,62 +1,92 @@
 import { Platform } from 'react-native';
-import { getInstanceId, getInstanceIdSync } from '../src';
+import * as RNDeviceInfo from '../src';
 import { clearMemo } from '../src/internal/supported-platform-info';
 let mockNativeModule = jest.requireMock('../src/internal/nativeInterface').default;
 
+function makeTable(name: string) {
+  return [
+    name, // name
+    (RNDeviceInfo as any)[name], // asyncGetter
+    (RNDeviceInfo as any)[`${name}Sync`], // syncGetter
+    mockNativeModule[name], // asyncNativeGetter
+    mockNativeModule[`${name}Sync`], // syncNativeGetter
+  ];
+}
+const memoizedStringGetters = [
+  'getInstanceId',
+  'getSerialNumber',
+  'getAndroidId',
+  'getBuildId',
+  'getInstallerPackageName',
+  'getDeviceName',
+  'getBootloader',
+  'getDevice',
+  'getDisplay',
+  'getFingerprint',
+  'getHardware',
+  'getHost',
+  'getProduct',
+  'getTags',
+  'getType',
+  'getBaseOs',
+  'getSecurityPatch',
+  'getCodename',
+  'getIncremental',
+  'getInstallReferrer',
+].map(makeTable);
+
 describe('string getters', () => {
-  describe('getInstanceId*', () => {
-    beforeEach(() => {
-      Platform.OS = 'android';
-    });
+  describe.each(memoizedStringGetters)(
+    '%s*',
+    (_name, asyncGetter, syncGetter, asyncNativeGetter, syncNativeGetter) => {
+      beforeEach(() => {
+        clearMemo();
+        Platform.OS = 'android';
+        asyncNativeGetter.mockClear();
+        syncNativeGetter.mockClear();
+      });
 
-    beforeEach(() => {
-      mockNativeModule.getInstanceIdSync.mockClear();
-      mockNativeModule.getInstanceId.mockClear();
-    });
+      it('should have an async version', () => {
+        expect(typeof asyncGetter).toBe('function');
+      });
 
-    it('should have an async version', () => {
-      expect(typeof getInstanceId).toBe('function');
-    });
+      it('should have a sync version', () => {
+        expect(typeof syncGetter).toBe('function');
+      });
 
-    it('should have a sync version', () => {
-      expect(typeof getInstanceIdSync).toBe('function');
-    });
+      it('should call native async module function', async () => {
+        const resp = await asyncGetter();
+        expect(resp).toEqual('unknown');
+        expect(asyncNativeGetter).toHaveBeenCalled();
+      });
 
-    it('should call RNDeviceInfo.getInstanceId', async () => {
-      clearMemo();
-      const resp = await getInstanceId();
-      expect(resp).toEqual('unknown');
-      expect(mockNativeModule.getInstanceId).toHaveBeenCalled();
-    });
+      it('should call native sync module function', () => {
+        const resp = syncGetter();
+        expect(resp).toEqual('unknown');
+        expect(syncNativeGetter).toHaveBeenCalled();
+      });
 
-    it('should call RNDeviceInfo.getInstanceIdSync', () => {
-      clearMemo();
-      const resp = getInstanceIdSync();
-      expect(resp).toEqual('unknown');
-      expect(mockNativeModule.getInstanceIdSync).toHaveBeenCalled();
-    });
+      it('should not call native sync module function on unsupported OS', () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = syncGetter();
+        expect(resp).toEqual('unknown');
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
 
-    it('should not call RNDeviceInfo.getInstanceIdSync on unsupported OS', () => {
-      clearMemo();
-      Platform.OS = 'ios';
-      const resp = getInstanceIdSync();
-      expect(resp).toEqual('unknown');
-      expect(mockNativeModule.getInstanceIdSync).not.toHaveBeenCalled();
-    });
+      it('should not call native async module function on unsupported OS', async () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = await asyncGetter();
+        expect(resp).toEqual('unknown');
+        expect(asyncNativeGetter).not.toHaveBeenCalled();
+      });
 
-    it('should not call RNDeviceInfo.getInstanceId on unsupported OS', async () => {
-      clearMemo();
-      Platform.OS = 'ios';
-      const resp = await getInstanceId();
-      expect(resp).toEqual('unknown');
-      expect(mockNativeModule.getInstanceId).not.toHaveBeenCalled();
-    });
-
-    it('should use memoized value if there exists one', async () => {
-      const resp = await getInstanceId();
-      const resp2 = getInstanceIdSync();
-      expect(resp).toBe(resp2);
-      expect(mockNativeModule.getInstanceIdSync).not.toHaveBeenCalled();
-    });
-  });
+      it('should use memoized value if there exists one', async () => {
+        const resp = await asyncGetter();
+        const resp2 = syncGetter();
+        expect(resp).toBe(resp2);
+        expect(asyncNativeGetter).toHaveBeenCalled();
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
+    }
+  );
 });
