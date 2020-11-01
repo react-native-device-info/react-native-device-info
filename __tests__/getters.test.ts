501654c40a09bfea90580c55f25b5c3c7d80e268
Dustin Schie <dustin@schie.io>
2020-10-31 20:47:53 -0400

    test(__tests__/getters): adding memoized number getters
    adding tests for the memoized number getters
    
    re #1108

--- __tests__/getters.test.ts
+++ __tests__/getters.test.ts
@@ -90,3 +90,68 @@ describe('string getters', () => {
     }
   );
 });
+
+const memoizedNumberGetters = [
+  'getApiLevel',
+  'getPreviewSdkInt',
+  'getFirstInstallTime',
+  'getLastUpdateTime',
+  'getTotalMemory',
+  'getMaxMemory',
+].map(makeTable);
+
+describe('number getters', () => {
+  describe.each(memoizedNumberGetters)(
+    '%s*',
+    (_name, asyncGetter, syncGetter, asyncNativeGetter, syncNativeGetter) => {
+      beforeEach(() => {
+        clearMemo();
+        Platform.OS = 'android';
+        asyncNativeGetter.mockClear();
+        syncNativeGetter.mockClear();
+      });
+
+      it('should have an async version', () => {
+        expect(typeof asyncGetter).toBe('function');
+      });
+
+      it('should have a sync version', () => {
+        expect(typeof syncGetter).toBe('function');
+      });
+
+      it('should call native async module function', async () => {
+        const resp = await asyncGetter();
+        expect(resp).toEqual(-1);
+        expect(asyncNativeGetter).toHaveBeenCalled();
+      });
+
+      it('should call native sync module function', () => {
+        const resp = syncGetter();
+        expect(resp).toEqual(-1);
+        expect(syncNativeGetter).toHaveBeenCalled();
+      });
+
+      it('should not call native sync module function on unsupported OS', () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = syncGetter();
+        expect(resp).toEqual(-1);
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
+
+      it('should not call native async module function on unsupported OS', async () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = await asyncGetter();
+        expect(resp).toEqual(-1);
+        expect(asyncNativeGetter).not.toHaveBeenCalled();
+      });
+
+      it('should use memoized value if there exists one', async () => {
+        const resp = await asyncGetter();
+        const resp2 = syncGetter();
+        expect(resp).toBe(resp2);
+        expect(asyncNativeGetter).toHaveBeenCalled();
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
+    }
+  );
+});
