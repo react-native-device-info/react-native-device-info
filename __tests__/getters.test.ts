9e8c5a505b2176557905266742116679cef292dd
Dustin Schie <dustin@schie.io>
2020-10-31 22:01:06 -0400

    test(__tests__/getters): adding memeoized array getters
    adding tests for the memoized array getters
    
    re #1108

--- __tests__/getters.test.ts
+++ __tests__/getters.test.ts
@@ -213,3 +213,76 @@ describe('boolean getters', () => {
     }
   );
 });
+
+const memoizedArrayGetters = [
+  [
+    'supported32BitAbis', // name
+    (RNDeviceInfo as any).supported32BitAbis, // asyncGetter
+    (RNDeviceInfo as any).supported32BitAbisSync, // syncGetter
+    mockNativeModule.getSupported32BitAbis, // asyncNativeGetter
+    mockNativeModule.getSupported32BitAbisSync, // syncNativeGetter
+  ],
+  [
+    'supported64BitAbis', // name
+    (RNDeviceInfo as any).supported64BitAbis, // asyncGetter
+    (RNDeviceInfo as any).supported64BitAbisSync, // syncGetter
+    mockNativeModule.getSupported64BitAbis, // asyncNativeGetter
+    mockNativeModule.getSupported64BitAbisSync, // syncNativeGetter
+  ],
+];
+
+describe('array getters', () => {
+  describe.each(memoizedArrayGetters)(
+    '%s*',
+    (_name, asyncGetter, syncGetter, asyncNativeGetter, syncNativeGetter) => {
+      beforeEach(() => {
+        clearMemo();
+        Platform.OS = 'android';
+        asyncNativeGetter.mockClear();
+        syncNativeGetter.mockClear();
+      });
+
+      it('should have an async version', () => {
+        expect(typeof asyncGetter).toBe('function');
+      });
+
+      it('should have a sync version', () => {
+        expect(typeof syncGetter).toBe('function');
+      });
+
+      it('should call native async module function', async () => {
+        const resp = await asyncGetter();
+        expect(resp).toEqual([]);
+        expect(asyncNativeGetter).toHaveBeenCalled();
+      });
+
+      it('should call native sync module function', () => {
+        const resp = syncGetter();
+        expect(resp).toEqual([]);
+        expect(syncNativeGetter).toHaveBeenCalled();
+      });
+
+      it('should not call native sync module function on unsupported OS', () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = syncGetter();
+        expect(resp).toEqual([]);
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
+
+      it('should not call native async module function on unsupported OS', async () => {
+        Platform.OS = 'GLaDOS' as any; // setting OS to something that won't match anything
+        const resp = await asyncGetter();
+        expect(resp).toEqual([]);
+        expect(asyncNativeGetter).not.toHaveBeenCalled();
+      });
+
+      it('should use memoized value if there exists one', async () => {
+        const resp = await asyncGetter();
+        const resp2 = syncGetter();
+        expect(resp).toBe(resp2);
+        expect(asyncNativeGetter).toHaveBeenCalled();
+        expect(syncNativeGetter).not.toHaveBeenCalled();
+      });
+    }
+  );
+});
