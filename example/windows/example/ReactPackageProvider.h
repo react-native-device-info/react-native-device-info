c4465010a96d6f11d2f3c71c39c719dfe019db3a
Mike Hardy <github@mikehardy.net>
2021-01-08 13:40:29 -0500

    chore(example): patch up refresh-example.sh and run it

--- example/windows/example/ReactPackageProvider.h
+++ example/windows/example/ReactPackageProvider.h
@@ -2,7 +2,6 @@
 
 #include "winrt/Microsoft.ReactNative.h"
 
-
 namespace winrt::example::implementation
 {
     struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
@@ -11,5 +10,4 @@ namespace winrt::example::implementation
         void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
     };
 } // namespace winrt::example::implementation
-
 
