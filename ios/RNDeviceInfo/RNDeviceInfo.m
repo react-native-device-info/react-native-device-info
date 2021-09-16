0bc979e687ef9ddbbd29410a60980cf536ce6002
Rolf Bjaanes <rolf@agens.no>
2021-09-16 17:25:15 +0200

    fix(hasNotch): Missing device name for iPhone 13 (#1309)
    * fix(hasNotch): Missing device name for iPhone 13
    
    Adds missing device name by code for iPhone 13 lineup. Relates to #1307
    Updated device code to name mapping to match the information in the sqlite3 device traits database provided with Xcode RC.

--- ios/RNDeviceInfo/RNDeviceInfo.m
+++ ios/RNDeviceInfo/RNDeviceInfo.m
@@ -240,11 +240,15 @@ RCT_EXPORT_METHOD(getDeviceName:(RCTPromiseResolveBlock)resolve rejecter:(RCTPro
         @"iPhone12,1": @"iPhone 11",
         @"iPhone12,3": @"iPhone 11 Pro",
         @"iPhone12,5": @"iPhone 11 Pro Max",
+	@"iPhone12,8": @"iPhone SE", // (2nd Generation iPhone SE),
         @"iPhone13,1": @"iPhone 12 mini",
         @"iPhone13,2": @"iPhone 12",
         @"iPhone13,3": @"iPhone 12 Pro",
         @"iPhone13,4": @"iPhone 12 Pro Max",
-        @"iPhone12,8": @"iPhone SE", // (2nd Generation iPhone SE)
+	@"iPhone14,4": @"iPhone 13 mini",
+	@"iPhone14,5": @"iPhone 13",
+        @"iPhone14,2": @"iPhone 13 Pro",
+        @"iPhone14,3": @"iPhone 13 Pro Max",
         @"iPad4,1": @"iPad Air", // 5th Generation iPad (iPad Air) - Wifi
         @"iPad4,2": @"iPad Air", // 5th Generation iPad (iPad Air) - Cellular
         @"iPad4,3": @"iPad Air", // 5th Generation iPad (iPad Air)
