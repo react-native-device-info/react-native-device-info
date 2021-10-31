7e6d931b03b2b3e60c1619685dde1012d302fb07
Dustin Schie <dustin@schie.io>
2021-10-30 20:38:08 -0400

    fix(internal/nativeinferface): fixing exception message when RNDeviceInfo is not found
    replaces mentions for `react-native-community` with `react-native-device-info`

--- src/internal/nativeInterface.ts
+++ src/internal/nativeInterface.ts
@@ -17,11 +17,11 @@ if (!RNDeviceInfo) {
     // @ts-ignore
     Platform.OS === 'dom'
   ) {
-    throw new Error(`@react-native-community/react-native-device-info: NativeModule.RNDeviceInfo is null. To fix this issue try these steps:
+    throw new Error(`react-native-device-info: NativeModule.RNDeviceInfo is null. To fix this issue try these steps:
   • For react-native <= 0.59: Run \`react-native link react-native-device-info\` in the project root.
   • Rebuild and re-run the app.
   • If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
-  If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-community/react-native-device-info`);
+  If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-device-info/react-native-device-info`);
   }
 }
 
